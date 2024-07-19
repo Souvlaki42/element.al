afd7a30bdc39b7c8d81cbd29b2ba21550e7c3389
Souvlaki42 <souvlaki420@gmail.com>
2024-07-19 17:38:36 +0300

    Made responsive

--- src/App.tsx
+++ src/App.tsx
@@ -47,21 +47,21 @@ const ElementDisplay = ({
   const handleExitClick = () => setDisplayInfo(false);
   const [exitOnHover, setExitOnHover] = createSignal<boolean>(false);
   return (
-    <main class="h-[100vh] w-[100vw] justify-center dark:bg-gray-900">
+    <main class="flex h-full w-full flex-col items-center overflow-hidden rounded-lg bg-white dark:bg-gray-900">
       <KeySequence sequenceHandler={konamiCodeKeySequenceHandler}></KeySequence>
-      <div class="flex h-[20vh] w-screen justify-end">
+      <div class="flex w-full justify-end bg-inherit p-4">
         <img
           src={exitOnHover() ? "exit2.svg" : "exit.svg"}
-          class="m-10 h-10 w-10"
+          class="h-10 w-10 cursor-pointer"
           alt="Exit"
           onClick={handleExitClick}
           onMouseEnter={() => setExitOnHover(true)}
           onMouseLeave={() => setExitOnHover(false)}
         />
       </div>
-      <div class="flex h-fit w-screen justify-center">
+      <div class="flex w-full justify-center bg-inherit p-4">
         <div
-          class="flex h-[150px] w-[150px] cursor-default flex-col items-center justify-center rounded-xl border-none transition-all duration-200 ease-in-out"
+          class="flex h-[150px] w-[150px] cursor-default flex-col items-center justify-center rounded-xl transition-all duration-200 ease-in-out"
           style={{ "background-color": `#${data?.cpkHexColor}` }}
         >
           <h1 class="font-poppins text-[1rem]">{data?.atomicMass}</h1>
@@ -69,38 +69,38 @@ const ElementDisplay = ({
           <h1 class="font-poppins text-[1rem]">{data?.atomicNumber}</h1>
         </div>
       </div>
-      <div class="flex h-[50vh] w-screen justify-center">
-        <div class="flex h-[50vh] w-[50vw] flex-col justify-center">
-          <p class="m-4 pl-[12.5vw] font-inter text-[2rem] dark:text-gray-200">
+      <div class="flex h-auto w-full flex-col justify-center bg-inherit p-4 md:flex-row">
+        <div class="flex w-full flex-col p-4 md:w-1/2">
+          <p class="m-4 font-inter text-lg md:text-2xl dark:text-gray-200">
             Atomic number : {data?.atomicNumber}
           </p>
-          <p class="m-4 pl-[12.5vw] font-inter text-[2rem] dark:text-gray-200">
+          <p class="m-4 font-inter text-lg md:text-2xl dark:text-gray-200">
             Symbol : {data?.symbol}
           </p>
-          <p class="m-4 pl-[12.5vw] font-inter text-[2rem] dark:text-gray-200">
+          <p class="m-4 font-inter text-lg md:text-2xl dark:text-gray-200">
             Name : {data?.name}
           </p>
-          <p class="m-4 pl-[12.5vw] font-inter text-[2rem] dark:text-gray-200">
+          <p class="m-4 font-inter text-lg md:text-2xl dark:text-gray-200">
             Atomic mass : {data?.atomicMass}
           </p>
-          <p class="m-4 pl-[12.5vw] font-inter text-[2rem] dark:text-gray-200">
+          <p class="m-4 font-inter text-lg md:text-2xl dark:text-gray-200">
             Electronic configuration : {data?.electronicConfiguration}
           </p>
         </div>
-        <div class="flex h-[50vh] w-[50vw] flex-col justify-center">
-          <p class="m-4 pl-[12.5vw] font-inter text-[2rem] dark:text-gray-200">
+        <div class="flex w-full flex-col p-4 md:w-1/2">
+          <p class="m-4 font-inter text-lg md:text-2xl dark:text-gray-200">
             Oxidation states : {data?.oxidationStates}
           </p>
-          <p class="m-4 pl-[12.5vw] font-inter text-[2rem] dark:text-gray-200">
+          <p class="m-4 font-inter text-lg md:text-2xl dark:text-gray-200">
             Standard physical state : {data?.standardState}
           </p>
-          <p class="m-4 pl-[12.5vw] font-inter text-[2rem] dark:text-gray-200">
+          <p class="m-4 font-inter text-lg md:text-2xl dark:text-gray-200">
             Density : {data?.density} g/cm^3
           </p>
-          <p class="m-4 pl-[12.5vw] font-inter text-[2rem] dark:text-gray-200">
+          <p class="m-4 font-inter text-lg md:text-2xl dark:text-gray-200">
             Type : {data?.groupBlock}
           </p>
-          <p class="m-4 pl-[12.5vw] font-inter text-[2rem] dark:text-gray-200">
+          <p class="m-4 font-inter text-lg md:text-2xl dark:text-gray-200">
             Year discovered : {data?.yearDiscovered}
           </p>
         </div>
@@ -120,14 +120,15 @@ const Home = ({
 
   return (
     <>
-      <header class="mb-8 text-center font-poppins text-6xl dark:text-white">
+      <header class="mb-8 text-center font-poppins text-4xl md:text-6xl dark:text-white">
         Element.al
       </header>
-      <main>
+      <main class="flex flex-col items-center">
         <KeySequence
           sequenceHandler={konamiCodeKeySequenceHandler}
         ></KeySequence>
         <form
+          class="flex flex-col items-center md:flex-row md:space-x-2"
           onSubmit={(e) => {
             e.preventDefault();
             if (!!!elementInput()) return;
@@ -148,7 +149,7 @@ const Home = ({
           }}
         >
           <input
-            class="h-[6vh] w-[30vw] rounded-md p-2 text-2xl"
+            class="h-[6vh] w-[80vw] rounded-md p-2 text-xl md:w-[30vw] md:text-2xl"
             type="text"
             name="element"
             placeholder="Search element"
@@ -157,18 +158,18 @@ const Home = ({
             required
           />
           <button
-            class="ml-2 h-[6vh] w-20 rounded-md border-none bg-[#4020cf] text-2xl text-white hover:bg-[#2f13ac]"
+            class="mt-4 h-[6vh] w-20 rounded-md border-none bg-[#4020cf] text-xl text-white hover:bg-[#2f13ac] md:mt-0 md:text-2xl"
             type="submit"
           >
             Go
           </button>
         </form>
-        <p class="m-3.5 text-center text-xl dark:text-gray-200">
+        <p class="m-3.5 text-center text-lg md:text-xl dark:text-gray-200">
           You can search by name, atomic number or symbol.
         </p>
       </main>
       <footer class="flex flex-col items-center">
-        <p class="mb-3.5 mt-8 text-center text-xl dark:text-gray-200">
+        <p class="mb-3.5 mt-8 text-center text-lg md:text-xl dark:text-gray-200">
           Created by
         </p>
         <div class="flex flex-row">
