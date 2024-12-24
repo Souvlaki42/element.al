ed0e188727c166295de359620e3a7aa25ca91cb1
Souvlaki42 <souvlaki420@gmail.com>
2024-12-24 13:59:26 +0200

    fix: Trying to fix production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel";
-
 import qwikdev from "@qwikdev/astro";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  site: "https://element.souviki.me",
   integrations: [tailwind(), qwikdev()],
   adapter: vercel()
 });
