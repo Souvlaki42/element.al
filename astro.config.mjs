6d680e899d1cd6ad2158b9a00f0c8f2d45b8b110
Souvlaki42 <souvlaki420@gmail.com>
2024-12-25 21:58:16 +0200

    fix: Trying to fix production 2
    Updated dependencies, changed output mode to server and fixed
    accessibility issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  site: "https://element.souviki.me",
+  output: "server",
+  site: "https://elemental.souviki.me",
   integrations: [tailwind(), qwikdev()],
   adapter: vercel()
 });
