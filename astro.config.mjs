bb67c92fd16f26066352b001d53b1f75a634191f
Souvlaki42 <souvlaki420@gmail.com>
2025-01-07 01:07:30 +0200

    fix: remove vercel astro adapter and changed to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 // @ts-check
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
 import qwikdev from "@qwikdev/astro";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   site: "https://elemental.souviki.me",
-  integrations: [tailwind(), qwikdev({ include: "**/qwik/*" })],
-  adapter: vercel({
-    imageService: true,
-    devImageService: "sharp"
-  })
+  integrations: [tailwind(), qwikdev({ include: "**/qwik/*" })]
 });
