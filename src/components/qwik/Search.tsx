37a38700ac5a43ad28e2c29b920fab0abbdc6ac0
Souvlaki42 <ilias@moulas.dev>
2026-04-03 19:30:41 +0300

    fix: update qwik imports

--- src/components/qwik/Search.tsx
+++ src/components/qwik/Search.tsx
@@ -1,4 +1,4 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$, useSignal } from "@qwik.dev/core";
 import type { InferEntrySchema } from "astro:content";
 type SearchProps = {
   elements: InferEntrySchema<"elements">[];
