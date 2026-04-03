261c945838d06bfec43953a4d33fa7370cd86f00
Souvlaki42 <ilias@moulas.dev>
2026-04-03 19:54:33 +0300

    chore: removed unneeded swup integration

--- astro.config.ts
+++ astro.config.ts
@@ -2,16 +2,12 @@
 import qwik from "@qwik.dev/astro";
 import { defineConfig } from "astro/config";
 
-import swup from "@swup/astro";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://elemental.moulas.dev",
-  integrations: [
-    qwik({ include: "**/qwik/*.tsx", clientRouter: true }),
-    swup()
-  ],
+  integrations: [qwik({ include: "**/qwik/*.tsx", clientRouter: true })],
   vite: {
     plugins: [tailwindcss()]
   }
