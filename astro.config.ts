39350b5ebffaf33ab3c43460553039246841b208
Souvlaki42 <ilias@moulas.dev>
2026-04-03 19:14:38 +0300

    fix: updated tailwind plugins and imported styles

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,15 @@
 // @ts-check
-import tailwind from "@astrojs/tailwind";
 import qwikdev from "@qwikdev/astro";
 import { defineConfig } from "astro/config";
 
 import swup from "@swup/astro";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://elemental.moulas.dev",
-  integrations: [tailwind(), qwikdev({ include: "**/qwik/*.tsx" }), swup()]
+  integrations: [qwikdev({ include: "**/qwik/*.tsx" }), swup()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
